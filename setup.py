3c3b3a6d4224149918acc54005a8375ef9de17c9
Your Name <you@example.com>
2024-04-24 10:28:16 +0800

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 PKG_NAME = "simular-eth-stubs"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 setup(
     name=PKG_NAME,
     version=VERSION,
     packages=['simular-stubs'],
-    description="Type stubs for simular-eth",
+    description="Type stubs for simular-evm",
     url=f"https://github.com/james4ever0/{PKG_NAME}",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
